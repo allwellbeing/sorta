bed3eb20520458f0413726bc1d7e5a1fb8e79e35
allwellbeing <118352649+allwellbeing@users.noreply.github.com>
2022-12-03 01:10:25 +0800

    Add files via upload

---
+++ render-template.js
@@ -0,0 +1,23 @@
+module.exports=function(obj) {
+obj || (obj = {});
+var __t, __p = '';
+with (obj) {
+__p += '<!DOCTYPE html><html ' +
+((__t = ( _meta.htmlAttrs )) == null ? '' : __t) +
+'><head>' +
+((__t = ( _meta.headTags )) == null ? '' : __t) +
+' <title>Quasar App</title><meta charset=utf-8><meta name=description content="A Quasar Project"><meta name=format-detection content="telephone=no"><meta name=msapplication-tap-highlight content=no><meta name=viewport content="initial-scale=1,width=device-width"><link rel=icon type=image/png sizes=128x128 href=/icons/favicon-128x128.png><link rel=icon type=image/png sizes=96x96 href=/icons/favicon-96x96.png><link rel=icon type=image/png sizes=32x32 href=/icons/favicon-32x32.png><link rel=icon type=image/png sizes=16x16 href=/icons/favicon-16x16.png><link rel=icon type=image/ico href=/favicon.ico><link rel=stylesheet href="https://fonts.googleapis.com/icon?family=Material+Icons"><script type=module crossorigin src=/assets/index.87fbe3f1.js></script><link rel=stylesheet href=/assets/index.d164ed76.css><link rel=manifest href=/manifest.json><meta name=theme-color content=#027be3><link rel=mask-icon href=/icons/safari-pinned-tab.svg color=#027be3><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=default><meta name=msapplication-TileImage content=/icons/ms-icon-144x144.png><meta name=msapplication-TileColor content=#000000><meta name=apple-mobile-web-app-title content="Quasar App"><link rel=apple-touch-icon href=/icons/apple-icon-120x120.png><link rel=apple-touch-icon sizes=152x152 href=/icons/apple-icon-152x152.png><link rel=apple-touch-icon sizes=167x167 href=/icons/apple-icon-167x167.png><link rel=apple-touch-icon sizes=180x180 href=/icons/apple-icon-180x180.png>' +
+((__t = ( _meta.endingHeadTags || '' )) == null ? '' : __t) +
+'</head><body class="' +
+((__t = ( _meta.bodyClasses )) == null ? '' : __t) +
+'" ' +
+((__t = ( _meta.bodyAttrs )) == null ? '' : __t) +
+'>' +
+((__t = ( _meta.bodyTags )) == null ? '' : __t) +
+' <div id=q-app>' +
+((__t = ( _meta.runtimePageContent )) == null ? '' : __t) +
+'</div></body></html>';
+
+}
+return __p
+}
